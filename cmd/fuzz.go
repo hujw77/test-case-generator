a8e9ac7a8f7706809585d83c2a42ef5371273cde
echo <hujw77@gmail.com>
2022-12-27 17:31:31 +0800

    fork from ethereum-op

--- cmd/fuzz.go
+++ cmd/fuzz.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"log"
 
-	t "github.com/ethereum-optimism/optimism/packages/contracts-bedrock/ctb-test-case-generator/trie"
+	t "github.com/hujw77/test-case-generator/trie"
 )
 
 // Mode enum
